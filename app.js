9d58bb0b8b6913ddf1fe15eb03cc1c8cb0e6894a
JCapito <joshuaecapito22@gmail.com>
2024-10-18 21:26:20 +0100

    feature: Implemented game grid + generate random blocks

--- app.js
+++ app.js
@@ -4,11 +4,30 @@ const square = 10;
 let squareArr = [];
 console.log(squareArr);
 
-function generateRandomBlock() {}
-
 for (let i = 0; i < square * square; i++) {
   let squareItems = document.createElement("div");
   squareItems.classList.add("grid-item");
   displayGrid.appendChild(squareItems);
   squareArr.push(squareItems);
 }
+
+function generateRandomBlock() {
+  if (squareArr.length === 0) return;
+  let randomIdx = Math.floor(Math.random() * squareArr.length);
+  let randomBlock = squareArr[randomIdx];
+  console.log(randomBlock);
+  if (randomBlock) {
+    randomBlock.classList.add("active-block");
+    randomBlock.style.backgroundColor = "red";
+  }
+}
+
+generateRandomBlock();
+generateRandomBlock();
+generateRandomBlock();
+generateRandomBlock();
+generateRandomBlock();
+generateRandomBlock();
+generateRandomBlock();
+generateRandomBlock();
+generateRandomBlock();
